281407ef3f075ceb508f4aa1d14942f5e2f47e8e
Felix-Asante <quamzgraphix@gmail.com>
2022-02-11 17:06:21 +0100

    layout,about,blog page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
 import Layout from "../src/components/Layout";
+import fs from "fs";
+import path from "path";
 export default function HomePage() {
 	return (
 		<Layout>
 			<p>Tailwind</p>
 		</Layout>
 	);
+}
+
+export async function getStaticProps() {
+	const files = fs.readdirSync(path.join("pages/posts"));
+	const post = files.map((filename) => {
+		const slug = filename.replace(".md", " ");
+
+		return {
+			slug,
+		};
+	});
+	console.log(post);
+	return {
+		props: {},
+	};
 }
