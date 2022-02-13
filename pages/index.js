dea93e1ade45b3f59fcc300fb2f47985f2458bc3
Felix-Asante <quamzgraphix@gmail.com>
2022-02-13 17:55:18 +0100

    homepage updated

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,44 @@
 import Layout from "../src/components/Layout";
 import fs from "fs";
 import path from "path";
-export default function HomePage() {
+import matter from "gray-matter";
+import Link from "next/link";
+import Post from "../src/components/Post";
+import sortByDate from "../src/utils/index";
+export default function HomePage({ posts }) {
 	return (
 		<Layout>
-			<p>Tailwind</p>
+			<h1 className="text-5xl border-b-4 p-5 font-bold">Latest Posts</h1>
+			<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+				{posts.map((post, index) => (
+					<Post key={index} post={post} />
+				))}
+			</div>
+			<Link href="/blog">
+				<a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
+					More Posts
+				</a>
+			</Link>
 		</Layout>
 	);
 }
 
 export async function getStaticProps() {
 	const files = fs.readdirSync(path.join("pages/posts"));
-	const post = files.map((filename) => {
+	const posts = files.map((filename) => {
 		const slug = filename.replace(".md", " ");
-
+		const markdownWithMeta = fs.readFileSync(
+			path.join("pages/posts", filename),
+			"utf-8"
+		);
+		const { data: frontmatter } = matter(markdownWithMeta);
 		return {
 			slug,
+			frontmatter,
 		};
 	});
-	console.log(post);
+
 	return {
-		props: {},
+		props: { posts: posts.sort(sortByDate).slice(0, 6) },
 	};
 }
